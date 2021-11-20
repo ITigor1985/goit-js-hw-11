a66bb2e846281375ce88c9080b9741adcf751265
Igor Tarasov <xromer85@gmail.com>
2021-11-20 23:59:46 +0300

    gl

--- src/js/axiosGetImage.js
+++ src/js/axiosGetImage.js
@@ -1,17 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
+const BASE_URL = 'https://pixabay.com/api/?key=24398479-635f1f2d5b3b2135c0a24be0a';
 
-const BASE_URL = 'https://pixabay.com/api/';
+// const parameters = {
+//   key: '24398479-635f1f2d5b3b2135c0a24be0a',
+//   q: 'cat',
+//   image_type: 'photo',
+//   orientation: 'horizontal',
+//   safesearch: 'true',
+//   per_page: 40,
+// };
 
-const parameters = {
-    key: "24398479-635f1f2d5b3b2135c0a24be0a",
-    q: "cat",
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: "true",
-    per_page: 40
+export default function getImg(q) {
+  return axios.get(
+    `${BASE_URL}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`,
+  );
 }
-
-export default function getImg (){
-    return axios.get(BASE_URL, parameters)          
-}  
